47506b82ac5d654db89bbcfaa974afd724b5955f
eliezerkenya <dev.eliezer.media@gmail.com>
2026-01-16 06:36:24 +0300

    first working

---
+++ api_server.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import json, os
+from a2wsgi import WSGIMiddleware
+
+class ApiServerAllocator(object):
+    def __init__(self, data={}):
+        self.version = "0.1"
+        self.data = data
+        self.__author__ = "eliezerkenya.net"
+        
+    def _api_encoder_(self):
+        json_inst = json.JSONEncoder()
+        json_inst = json_inst.encode(self.data)
+        return json_inst
+    
+    def _serv_decorder_(self):
+        pass
+    
+class LiveServiceProvider(object):
+    data = {}
+    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'u,i-5"k-|90')  
+    
+    @app.route("/")
+    def index():
+        return "This server is temporarily moved and outdated, out of service but expected to be opened soon, thank you"
+    
+    @app.route("/main")
+    def main():
+        with open("api-json-samp.json", "r") as rd:
+            rd = {"feed": rd.read()}
+        return rd
+            
+            
+    @app.route("/api/api-post-req/serv/api", methods=["POST"])
+    def get_my_api():
+        get = ApiServerAllocator(data=LiveServiceProvider.data)
+        data = get._api_encoder_()
+        return data
+    
+app = WSGIMiddleware(LiveServiceProvider().app)
